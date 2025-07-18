30567dc9adcc7b25322c67f2c574ff432d30f8ce
Valeriy Shestakov <shestakovvvaleriy@gmail.com>
2025-07-18 21:28:13 +0300

    initial

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,15 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0,
+  }),
+  actions: {
+    increment() {
+      this.count++
+    },
+    reset() {
+      this.count = 0
+    },
+  },
 })
